17e8948939dd5f63a1ea2bc8360aae878beee0c3
Arun <46720436+arun2728@users.noreply.github.com>
2024-05-16 16:07:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,18 @@ import time
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def hello_world():
     # return render_template("index.html")
+    print("Got request...")
     start_time = time.time()
     print("Task started at", start_time)
     # Simulate a long-running task
     time.sleep(60 * 15)  # Sleep for 15 minutes
     end_time = time.time()
     print("Task completed at", end_time)
-    return jsonify({'message': 'Task completed successfully'})
+    #return jsonify({'message': 'Task completed successfully'})
+    return render_template("index.html")
     
 @app.route('/long_running_task', methods=['GET'])
 def long_running_task():
